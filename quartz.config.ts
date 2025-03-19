703938cb8e02284fd4659db267e44aeea1cda9e7
Babu Macharia <babu.macharia@gmail.com>
2025-03-18 21:02:23 -0700

    update config file

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "The DevOps Protagonist",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
