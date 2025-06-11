ae6414dede58e2623340777fec34eef89b37825e
Babu Macharia <babu.macharia@gmail.com>
2025-06-10 18:39:27 -0700

    Quartz sync: Jun 10, 2025, 6:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Cloud & Container Musings: A DevOps Diary"
+    pageTitle: "Cloud & Container Musings: A DevOps Journal"
 
 ,
     pageTitleSuffix: "",
